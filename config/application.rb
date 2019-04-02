18a67e0a512eb87a9514c6c70330b449742b5410
Demetrio Cruz <hesenrre@gmail.com>
2019-04-02 00:25:38 -0600

    added lib/classes to loadpath

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Crawler
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.autoload_paths << Rails.root.join("lib").join("classes")
+    config.eager_load_paths << Rails.root.join("lib").join("classes")
   end
 end
